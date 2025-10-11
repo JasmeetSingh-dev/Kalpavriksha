eeb579583da2800ddd089c150149c1a10ca4a36d
JasmeetSingh-dev <jassi1008008@gmail.com>
2025-10-11 23:56:08 +0530

    Update Calculator.c
    Add three functions namely: parseExpression, evaluateMD and evaluateAS.

--- Assignment-1/Calculator.c
+++ Assignment-1/Calculator.c
@@ -3,7 +3,70 @@
 #include <string.h>
 #include <ctype.h>
 
-int MAX = 100;
+#define MAX 100
+
+int parseExpression(const char *expression, int values[], char ops[], int *vtop, int *otop) {
+    int i = 0;
+    while (expression[i] != '\0') {
+        if (isspace(expression[i])) {
+            i++;
+            continue;
+        }
+
+        if (isdigit(expression[i])) {
+            int val = 0;
+            while (isdigit(expression[i])) {
+                val = val * 10 + (expression[i] - '0');
+                i++;
+            }
+            values[++(*vtop)] = val;
+        } 
+        else if (strchr("+-*/", expression[i])) {
+            ops[++(*otop)] = expression[i];
+            i++;
+        } 
+        else {
+            printf("Invalid character '%c' in expression.\n", expression[i]);
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void evaluateMD(int values[], char ops[], int *vtop, int *otop) {
+    for (int j = 0; j <= *otop; j++) {
+        if (ops[j] == '*' || ops[j] == '/') {
+            if (ops[j] == '*') {
+                values[j] = values[j] * values[j + 1];
+            } else {
+                if (values[j + 1] == 0) {
+                    printf("Division by zero error!\n");
+                    exit(1);
+                }
+                values[j] = values[j] / values[j + 1];
+            }
+
+            for (int k = j + 1; k < *vtop; k++) {
+                values[k] = values[k + 1];
+            }
+            for (int k = j; k < *otop; k++) {
+                ops[k] = ops[k + 1];
+            }
+            (*vtop)--;
+            (*otop)--;
+            j--; 
+        }
+    }
+}
+
+int evaluateAS(int values[], char ops[], int vtop, int otop) {
+    int res = values[0];
+    for (int j = 0; j <= otop; j++) {
+        if (ops[j] == '+') res += values[j + 1];
+        else if (ops[j] == '-') res -= values[j + 1];
+    }
+    return res;
+}
 
 int main() {
     char expression[MAX];
@@ -13,58 +76,16 @@ int main() {
 
     printf("Enter expression: ");
     fgets(expression, MAX, stdin);
-    expression[strcspn(expression, "\n")] = 0;
+    expression[strcspn(expression, "\n")] = 0; 
 
-    int i = 0;
-    while (expression[i] != '\0') {
-        if (expression[i] == ' ') { 
-            i++; 
-            continue; 
-        }
-        if (isdigit(expression[i])) {
-            int val = 0;
-            while (isdigit(expression[i])) {
-                val = val * 10 + (expression[i] - '0');
-                i++;
-            }
-            values[++vtop] = val;
-        } else if (strchr("+-*/", expression[i])) {
-            ops[++otop] = expression[i];
-            i++;
-        } else {
-            printf("Invalid character in expression.\n");
-            return 1;
-        }
+    if (!parseExpression(expression, values, ops, &vtop, &otop)) {
+        return 1;
     }
 
-    for (int j = 0; j <= otop; j++) {
-        if (ops[j] == '*' || ops[j] == '/') {
-            if (ops[j] == '*') values[j] = values[j] * values[j+1];
-            else {
-                if (values[j+1] == 0) {
-                    printf("Division by zero error!\n");
-                    return 1;
-                }
-                values[j] = values[j] / values[j+1];
-            }
-            for (int k = j+1; k < vtop; k++) {
-                values[k] = values[k+1];
-            }
-            for (int k = j; k < otop; k++) {
-                ops[k] = ops[k+1];
-            }
-            vtop--; 
-            otop--; 
-            j--;
-        }
-    }
+    evaluateMD(values, ops, &vtop, &otop);
 
-    int res = values[0];
-    for (int j = 0; j <= otop; j++) {
-        if (ops[j] == '+') res += values[j+1];
-        else if (ops[j] == '-') res -= values[j+1];
-    }
+    int result = evaluateAS(values, ops, vtop, otop);
 
-    printf("Result: %d\n", res);
+    printf("Result: %d\n", result);
     return 0;
 }
